318be1392cfd5eefa0ff31744ff12774ad54cf90
Mateus Farias <mateusfariasz86@gmail.com>
2022-04-05 17:28:22 -0300

    Added routes in the project

---
+++ src/app/app-mock.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Adam Warlock' },
+    { id: 12, name: 'Albion' },
+    { id: 13, name: 'APOCALYPSE' },
+    { id: 14, name: 'JIGSAW' },
+    { id: 15, name: 'Betsy Ross' },
+    { id: 16, name: 'Carol Danvers' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Groot' },
+    { id: 19, name: 'Jennifer Walters' },
+    { id: 20, name: 'Rose' }
+];
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
